bf66347e922fc4d8d14ef8bcb72471bdd694c39e
RunningDinnerProgramming <105112327+RunningDinnerProgramming@users.noreply.github.com>
2022-05-07 23:00:00 +0100

    Update Run.py

--- Run.py
+++ Run.py
@@ -235,12 +235,12 @@ else:
     output1["Group"] = output1["Group"].astype(int)
     
     if  output1[output1['Group']] == 1:
-        output1.loc[output1['Group'] == 1] = "Appetizer"
+        output1[output1['Group']] = "Appetizer"
     elif output1[output1['Group']] == 2:
-        output1.loc[output1['Group'] == 2] = "Main Course"
+        output1[output1['Group']] = "Main Course"
     else:
-        output1.loc[output1['Group'] == 3] = "Dessert"
-        
+        output1[output1['Group']] = "Dessert"
+    #output1.loc[output1['Group'] == 3]    
     output1 = output1.drop(columns=["Zeitstempel","TeamID"])
     
     all_teams=["All"]

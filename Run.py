871adb5756575d0ede48f8d7d220c110b302c781
RunningDinnerProgramming <105112327+RunningDinnerProgramming@users.noreply.github.com>
2022-05-07 23:47:11 +0100

    Update Run.py

--- Run.py
+++ Run.py
@@ -291,7 +291,7 @@ else:
         
         #get teams that submitted to late
         if lost_Data.empty == False:
-            st.write("""### Lost Data:""")
+            st.write("""### Late Submitters:""")
             #st.session_state = output
             hide_dataframe_row_index = """
                                             <style>
@@ -341,7 +341,7 @@ else:
         
         #get teams that submitted to late
         if lost_Data.empty == False:
-            st.write("""### Lost Data:""")
+            st.write("""### Late Submitters:""")
             hide_dataframe_row_index = """
                                             <style>
                                             .row_heading.level0 {display:none}

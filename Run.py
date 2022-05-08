98dd1d0fe656138742560cfa5caad6e82cc8fe88
RunningDinnerProgramming <105112327+RunningDinnerProgramming@users.noreply.github.com>
2022-05-08 18:05:57 +0100

    Update Run.py

--- Run.py
+++ Run.py
@@ -140,7 +140,7 @@ class algorithm:
         wrong_address = dataT[(dataT["latitude"] == -89.9999) & (dataT["longitude"] == -179.9999)]
 
         if wrong_address.shape[0] != 0:
-            dataT.drop(index=list(dataT[(dataT["latitude"] == -89.9999) & (dataT["longitude"] == -179.9999)].index))
+            dataT.drop(index=list(dataT[(dataT["latitude"] == -89.9999) & (dataT["longitude"] == -179.9999)].index), inplace = True)
         else:
             dataT=dataT
         
